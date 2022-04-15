de4edff72d57e70f0ab85094650ca024c4174ee1
David Woo <dwoo1016@gmail.com>
2022-04-15 01:27:16 -0400

    first commit

---
+++ src/Components/HomePage.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './HomePage.css'
+
+const HomePage = () => {
+    return (
+        <div className='hp'>
+            <div className='hp-wrapper'>
+                {/* <div className='hp-name'>Hi there, my name's David Woo and I am a</div> */}
+                <div className='hp-name'>David Woo</div>
+                <div className='hp-title1'>Software Engineer</div>
+                <div className='hp-title2'>Software Developer</div>
+            </div>
+        </div>
+    )
+}
+
+export default HomePage
