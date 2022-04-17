9b47712a9a59cf09b347334a2b652bad23c260f2
David Woo <dwoo1016@gmail.com>
2022-04-17 17:31:12 -0400

    updated Projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,20 +2,22 @@ import React from 'react'
 import './Projects.css'
 import Project1 from '../unplgd.jpg'
 import Project2 from '../podify.png'
+import Project3 from '../proj3.jpg'
 
 const Projects = () => {
     return (
         <div className='pj'>
+            <div className='pj-title'>
+                <h3>Current Projects</h3>
+            </div>
             <div className='pj-wrapper'>
-                <div className='pj-container'>
-                    <div className='pj-title'>
-                        <h3>Current Projects</h3>
-                    </div>
-                    <div className='pj-img-wrapper'>
-                        <img className='pj-img' alt='' src={Project1}/>
-                        <img className='pj-img' alt='' src={Project2}/>
-                        <img className='pj-img' alt='' src={Project1}/>
-                    </div>
+                <div className='pj-img-wrapper'>
+                    <img className='pj-img' alt='' src={Project1}/>
+                    <img className='pj-img' alt='' src={Project2}/>
+                    {/* <img className='pj-img' alt='' src={Project3}/> */}
+                </div>
+                <div className='pj-img-right'>
+                    <img className='pj-img-tall' alt='' src={Project3}/>
                 </div>
             </div>
         </div>
