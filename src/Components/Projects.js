662a6e1fe5bc1f960389548ff41bd9ba8bcc6770
David Woo <dwoo1016@gmail.com>
2022-04-17 15:59:38 -0400

    updated homepage

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -7,12 +7,14 @@ const Projects = () => {
     return (
         <div className='pj'>
             <div className='pj-wrapper'>
-                <div className='pj-title'>
-                    Current Projects
-                </div>
-                <div className='pj-img-wrapper'>
-                    <img className='pj-img' alt='' src={Project1}/>
-                    <img className='pj-img' alt='' src={Project2}/>
+                <div className='pj-container'>
+                    <div className='pj-title'>
+                        <h2>Current Projects</h2>
+                    </div>
+                    <div className='pj-img-wrapper'>
+                        <img className='pj-img' alt='' src={Project1}/>
+                        <img className='pj-img' alt='' src={Project2}/>
+                    </div>
                 </div>
             </div>
         </div>
