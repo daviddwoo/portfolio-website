b646fc3eb56f5e839d635780f2824fc5df72b0ca
David Woo <dwoo1016@gmail.com>
2022-04-17 16:57:19 -0400

    updated Contact

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import HomePage from './Components/HomePage'
 import Projects from './Components/Projects'
+import Contact from './Components/Contact';
 
 function App() {
   return (
     <div>
       <HomePage />
       <Projects />
+      <Contact />
     </div>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import './Contact.css'
+import { TextField } from '@mui/material'
+import { styled } from '@mui/material';
+
+const CssTextField = styled(TextField)({
+    '& label.Mui-focused': {
+      color: 'black',
+    },
+    '& .MuiInput-underline:after': {
+      borderBottomColor: 'black',
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: 'black',
+      },
+      '&:hover fieldset': {
+        borderColor: 'black',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: 'black',
+      },
+    },
+});
+  
 
 const Contact = () => {
     return (
-        <div>
+        <div className='ct'>
             Contact Form
+            <div className='ct-wrapper'>
+                <CssTextField  className='field' label="Name" variant="outlined" />
+                <CssTextField  className='field' label="Email" variant="outlined" />
+                <CssTextField  className='field' label="Message" variant="outlined" />
+            </div>
         </div>
     )
 }
