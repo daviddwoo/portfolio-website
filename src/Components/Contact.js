1d44f88e8614f3f6260e402cd7bfb95f657216ce
David Woo <dwoo1016@gmail.com>
2022-04-18 14:20:40 -0400

    updated Contact styling

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,10 +1,9 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import './Contact.css'
 import { TextField } from '@mui/material'
 import { styled } from '@mui/material';
 import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
-// import emailjs from '@emailjs/browser'
 import emailjs from 'emailjs-com'
 
 const CssTextField = styled(TextField)({
@@ -33,92 +32,109 @@ const CssTextField = styled(TextField)({
 const Contact = () => {
 
     const form = useRef();
+    const [formData, setFormData] = useState({'from_name': '', 'from_email': '', 'message': ''})
 
     const onSubmit = (ev) => {
         ev.preventDefault();
         emailjs.sendForm(process.env.REACT_APP_SERVICE_ID, process.env.REACT_APP_TEMPLATE_ID, form.current, process.env.REACT_APP_PUBLIC_KEY)
             .then((res) => {
-                console.log(res.text)
+                // console.log(res.text)
             }, (err) => {
                 console.log(err.text)
-            })
+            }
+        )
+        setFormData({'from_name': '', 'from_email': '', 'message': ''})
+    }
+
+    const onChange = (ev) => {
+        const change = formData;
+        change[ev.target.name] = ev.target.value
+        setFormData({...change, [ev.target.name]: ev.target.value})
     }
 
     return (
         <div className='ct'>
-            <div className='ct-title'>
-                <h3>Get in touch</h3>
-                <span>Feel free to leave a message,</span>
-                <span>or connect with me on LinkedIn!</span>
-            </div>
-            <div className='ct-form'>
-                <form ref={form} onSubmit={onSubmit}>
-                    <div className='ct-field'>
-                        <CssTextField
-                            required
-                            type='text'
-                            className='field' 
-                            label="Name"
-                            name='from_name' 
-                            variant="outlined"     
-                            inputProps={{
-                                style: {
-                                    width: '640px'
+            <div className='ct-wrapper'>
+                <div className='ct-title'>
+                    <h3>Get in touch</h3>
+                    <span>Feel free to leave a message</span>
+                    <span>or connect with me on LinkedIn!</span>
+                </div>
+                <div className='ct-form'>
+                    <form ref={form} onSubmit={onSubmit}>
+                        <div className='ct-field'>
+                            <CssTextField
+                                onChange={onChange}
+                                required
+                                type='text'
+                                className='field' 
+                                label="Name"
+                                name='from_name' 
+                                variant="outlined"     
+                                inputProps={{
+                                    style: {
+                                        width: '640px'
+                                    }
+                                }}
+                                value={formData['from_name']}
+                            />
+                        </div>
+                        <div className='ct-field'>
+                            <CssTextField
+                                onChange={onChange}
+                                required 
+                                type='email' 
+                                className='field' 
+                                label="Email"
+                                name='from_email' 
+                                variant="outlined"     
+                                inputProps={{
+                                    style: {
+                                        width: '640px'
+                                    }
+                                }}
+                                value={formData['from_email']}
+                            />
+                        </div>
+                        <div className='ct-field'>
+                            <CssTextField
+                                onChange={onChange} 
+                                required
+                                className='field' 
+                                label="Message"
+                                name='message' 
+                                multiline
+                                rows={4}
+                                variant="outlined"     
+                                inputProps={{
+                                    style: {
+                                        height: '100px',
+                                        width: '640px',
+                                    }
+                                }}
+                                value={formData['message']}
+                            />
+                        </div>
+                        <Button
+                            type='submit' 
+                            variant='contained' 
+                            size='large' 
+                            endIcon={<SendIcon />} 
+                            sx={{
+                                backgroundColor: '#3d3d40', 
+                                width: '670px', 
+                                marginLeft: '15px',
+                                borderRadius: 0,
+                                '&:hover': {
+                                    backgroundColor: '#3d3d40',
+                                    borderRadius: 0
                                 }
                             }}
-                        />
-                    </div>
-                    <div className='ct-field'>
-                        <CssTextField
-                            required 
-                            type='email' 
-                            className='field' 
-                            label="Email"
-                            name='from_email' 
-                            variant="outlined"     
-                            inputProps={{
-                                style: {
-                                    width: '640px'
-                                }
-                            }}
-                        />
-                    </div>
-                    <div className='ct-field'>
-                        <CssTextField 
-                            required
-                            className='field' 
-                            label="Message"
-                            name='message' 
-                            multiline
-                            rows={4}
-                            variant="outlined"     
-                            inputProps={{
-                                style: {
-                                    height: '100px',
-                                    width: '640px',
-                                }
-                            }}
-                        />
-                    </div>
-                    <Button
-                        type='submit' 
-                        variant='contained' 
-                        size='large' 
-                        endIcon={<SendIcon />} 
-                        sx={{
-                            backgroundColor: '#3d3d40', 
-                            width: '670px', 
-                            marginLeft: '15px',
-                            borderRadius: 0,
-                            '&:hover': {
-                                backgroundColor: '#3d3d40',
-                                borderRadius: 0
-                            }
-                        }}
-                    >
-                        Send Message
-                    </Button>
-                </form>
+                        >
+                            Send Message
+                        </Button>
+                    </form>
+                </div>
             </div>
         </div>
     )
