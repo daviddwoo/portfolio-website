36ee87e660366926efe660a9a88df8a8adbe8f13
David Woo <dwoo1016@gmail.com>
2022-05-28 13:54:01 -0400

    update images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import './App.css';
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppRoutes from './Components/AppRoutes'
 import Navbar from './Components//Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import AnimatedCursor from 'react-animated-cursor'
+import Picture from './Screenshots/profile.webp'
+import projImg1 from './Screenshots/unplgd-hp.webp'
+import projImg2 from './Screenshots/podify-hp.webp'
+import projImg3 from './Screenshots/Conductor.webp'
 
 function App() {
+
+  const cacheImages = async(images) => {
+    const promises = await images.map((image) => {
+      return new Promise((res, rej) => {
+        const img = new Image();
+
+        img.src = image;
+        img.onload = res();
+        img.onerror = rej();
+      })
+    });
+
+    await Promise.all(promises);
+  }
+
+  useEffect(() => {
+
+    const images = [Picture, projImg1, projImg2, projImg3];
+
+    cacheImages(images);
+
+  }, []);
+
   return (
     <div>
       <AnimatedCursor 
