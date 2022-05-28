e34ad6f492872ff1c4281b2fc8b8424ad1c2e258
David Woo <dwoo1016@gmail.com>
2022-05-28 16:09:24 -0400

    updated framer

--- src/framer.js
+++ src/framer.js
@@ -1,24 +1,24 @@
 export const animations = {
     initial: {
       opacity: 0,  
-      y: 40,
+      y: 50,
       transition: { ease: [0.16, 1, 0.3, 1], duration: 1}
     },
     animate: {
       opacity: 1,
       y: 0,
-      transition: { ease: [0.16, 1, 0.3, 1], duration: 1.4}
+      transition: { ease: [0.16, 1, 0.3, 1], duration: 1.2}
     }
 };
 
 export const heroAnimation = {
   initial: {
     y: 400,
-    transition: { ease: [0.6, 0.01, -0.05, 1], duration: 1.8 }
+    transition: { ease: [0.6, 0.01, -0.05, 1], duration: 1.5 }
   },
   animate: {
     y: 0,
-    transition: { ease: [0.6, 0.01, -0.05, 1], duration: 1.8}
+    transition: { ease: [0.6, 0.01, -0.05, 1], duration: 1.5}
   }
 };
 
