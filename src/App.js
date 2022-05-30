6a719e28b3ac35c16efe93f549b5d4a3cfcb471c
David Woo <dwoo1016@gmail.com>
2022-05-29 23:33:42 -0400

    update img cache

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
 import './App.css';
-import React from 'react'
+import React, { useEffect } from 'react'
 import AppRoutes from './Components/AppRoutes'
 import Navbar from './Components//Navbar/Navbar';
 import Footer from './Components/Footer/Footer';
 import AnimatedCursor from 'react-animated-cursor'
-// import Picture from './Screenshots/profile.webp'
-// import projImg2 from './Screenshots/podify-hp.webp'
-// import projImg3 from './Screenshots/Conductor.webp'
+import Picture from './Screenshots/profile1.webp'
+import gshp1 from './Screenshots/gs-hp1.webp'
+import gshp2 from './Screenshots/gs-hp2.webp'
+import podify1 from './Screenshots/podify1.webp'
+import podify2 from './Screenshots/podify2.webp'
+import conductor1 from './Screenshots/Conductor1.webp'
+import conductor2 from './Screenshots/Conductor2.webp'
 
 function App() {
 
-  // const cacheImages = async(images) => {
-  //   const promises = await images.map((image) => {
-  //     return new Promise((res, rej) => {
-  //       const img = new Image();
+  const cacheImages = async(images) => {
+    const promises = await images.map((image) => {
+      return new Promise((res, rej) => {
+        const img = new Image();
 
-  //       img.src = image;
-  //       img.onload = res();
-  //       img.onerror = rej();
-  //     })
-  //   });
+        img.src = image;
+        img.onload = res();
+        img.onerror = rej();
+      })
+    });
+    await Promise.all(promises);
+  }
 
-  //   await Promise.all(promises);
-  // }
+  useEffect(() => {
 
-  // useEffect(() => {
+    const images = [Picture, gshp1, gshp2, podify1, podify2, conductor1, conductor2];
 
-  //   const images = [Picture, projImg2, projImg3];
+    cacheImages(images)
 
-  //   console.log(cacheImages(images));
-
-  // }, []);
+  }, []);
 
   return (
     <div>
